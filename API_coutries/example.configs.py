0ca884fea4883329d46a0116580bbb042e505b58
federicocalvette <calvettefederico@gmail.com>
2022-10-06 10:26:48 -0300

    Add example creds

---
+++ API_coutries/example.configs.py
@@ -0,0 +1,8 @@
+PERSONAL_TOKEN = ''
+USER_EMAIL = 'calvwrico@gmail.com'
+
+
+#URLs API Countries
+URL_GET_ACCESS_TOKEN = "https://www.universal-tutorial.com/api/getaccesstoken"
+URL_GET_STATES = "https://www.universal-tutorial.com/api/states/"
+URL_GET_CITIES = "https://www.universal-tutorial.com/api/cities/"
